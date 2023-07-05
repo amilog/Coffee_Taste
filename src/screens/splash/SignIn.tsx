713c33397a3aef8212c03cc4cb46d446e8d89751
amilgasimov <93605485+amilgasimov@users.noreply.github.com>
2023-07-05 23:20:47 +0400

    .

--- src/screens/splash/SignIn.tsx
+++ src/screens/splash/SignIn.tsx
@@ -48,7 +48,7 @@ const SignIn = () => {
 
     const onSubmit = handleSubmit(data => {
         axios
-            .post('http://169.254.159.183:8080/api/login', {
+            .post('http://192.168.1.107:8080/api/login', {
                 username: data.username,
                 password: data.password,
             })
